38a6a5d79abca8de089df063474210a72d5b0dba
lynncoleart <lynncoleart@users.noreply.github.com>
2026-05-31 18:23:05 -0500

    Update CLI scaffold for Zig 0.16

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,9 @@ const std = @import("std");
 const shovelerdb = @import("shovelerdb");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("{s}: {s}, implemented in {s}\n", .{
+    std.debug.print("{s}: {s}, implemented in {s}\n", .{
         shovelerdb.Project.name,
         shovelerdb.Project.dialect,
         shovelerdb.Project.implementation_language,
     });
 }
-
